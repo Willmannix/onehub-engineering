008f6de52576c5d797542ceabedd2bc4c851ea02
william mannix <william.mannix@genesys.com>
2024-06-16 22:49:44 +0100

    Initial commit: Add Spring Boot project for OneHub Engineering

---
+++ src/main/java/com/onehub/engineering/OneHubApplication.java
@@ -0,0 +1,11 @@
+package com.onehub.engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneHubApplication.class, args);
+    }
+}
